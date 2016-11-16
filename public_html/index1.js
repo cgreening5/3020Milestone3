2cd5efa4fdfdfa3e364e793b032fe83e76bd8531
cgreening <cgreening5@hotmail.com>
2016-11-16 14:03:51 -0600

    First commit.

---
+++ public_html/index1.js
@@ -0,0 +1,334 @@
+$(document).ready(function () {
+
+    var lastNav = null;	//last navigation button clicked
+    var lastMenu = $("#appetizer_button"); //last menu button clicked
+    var regColor = "DarkSlateGray"; //regular non highlighted button color
+    var hilite = "DodgerBlue"; //highlighted button color
+    var disabled = "DarkGray"; //disabled button color
+    var orderSubmitted = false; //order has been submitted = true
+    var orderCount = 0; //number of people ordering
+    var itemCount = 0; //number of distinct food items ordered
+    var currPage = $("#welcome"); //current page being shown
+    var prevPage = null; //prev page shown
+    var lastMenuScreen = $(".appetizers"); //last menu screen shown
+    var lastExpandScreen = null; //last expanded menu item expanded
+    var currItem = null; //current menu item to add to order
+    var itemOrderNote = null; //to keep track of ordering notifications
+    var maxItems = 9; //max items in an order
+    var orderBtnDisable = false; //disables buttons in the order screen if true
+    var naviBtnDisable = false; //disables navigation buttons if true
+
+    var firstOrder = new LinkedList(); //linked list of orders
+    var secondOrder = new LinkedList();
+    var thirdOrder = new LinkedList();
+    var fourthOrder = new LinkedList();
+
+    $(lastMenu).css("background-color", hilite);	//initial tab of menu is appetizers
+
+    //hide everything except welcome screen and navigation buttons
+    updateButtons();
+    $("#menu_screen").children().hide();
+    $(".appetizers").show();
+    $(".ordered").hide();
+    $("#menu_buttons").show();
+    $("#menu_port").show();
+    $("#menu_screen").hide();
+    $("#orders_screen").children().hide();
+    $("#order_one").show();
+    $("#new_order_two").show();
+    $("#submit_order").show();
+    $("#orders_screen").hide();
+    $("#expanded_window").children().hide();
+    $("#expand_screen").hide();
+    $("#order_submitted").hide();
+
+
+
+    //highlights menu button clicked, hides previous navigation screen, shows the menu screen
+    $("#menu").click(function () {
+        if (!naviBtnDisable) {
+            lastNav = $("#menu").clickHilite(lastNav);
+            prevPage = currPage;
+            currPage = $("#menu_screen").clickShow(currPage);
+        }
+        ;
+    });
+
+    //highlights orders button clicked, hides previous navigation screen, shows the orders screen, needs at least one item ordered
+    $("#order").click(function () {
+
+        var order_panel = $("#order1");
+        if (orderCount > 0 && !naviBtnDisable) {
+            lastNav = $("#order").clickHilite(lastNav);
+            prevPage = currPage;
+            currPage = $("#orders_screen").clickShow(currPage);
+        }
+        ;
+    });
+
+    //highlights bills button clicked, hides previous navigation screen, shows the bills screen, needs to submit order first
+    $("#bills").click(function () {
+        if (orderSubmitted && !naviBtnDisable) {
+            lastNav = $("#bills").clickHilite(lastNav);
+            $(currPage).hide();
+            prevPage = currPage;
+            currPage = $("#bills_screen").clickShow(currPage);
+        }
+        ;
+    });
+
+
+
+
+    //highlights most recently clicked menu buttons, shows current menu, hides previous menu
+    $("#appetizer_button").click(function () {
+        lastMenu = $("#appetizer_button").clickHilite(lastMenu); //highlight appetizer button 
+        lastMenuScreen = $(".appetizers").clickShow(lastMenuScreen); //switch to appetizer menu
+    });
+    $("#drinks_button").click(function () {
+        lastMenu = $("#drinks_button").clickHilite(lastMenu);
+        lastMenuScreen = $(".drinks").clickShow(lastMenuScreen);
+    });
+    $("#entree_button").click(function () {
+        lastMenu = $("#entree_button").clickHilite(lastMenu);
+        lastMenuScreen = $(".entree").clickShow(lastMenuScreen);
+    });
+    $("#dessert_button").click(function () {
+        lastMenu = $("#dessert_button").clickHilite(lastMenu);
+        lastMenuScreen = $(".dessert").clickShow(lastMenuScreen);
+    });
+
+
+
+
+    //expands menu item when clicked
+    $("#fried_calamari").click(function () {
+        lastExpandScreen = $("#fried_calamari_e").clickShow(lastExpandScreen);
+        $(currPage).hide();
+        prevPage = currPage;
+        currPage = $("#expand_screen").clickShow(currPage);
+        currItem = "Fried Calamari";
+        itemOrderNote = "#calamari_ordered";
+        ;
+    });
+
+    $("#crisper").click(function () {
+        lastExpandScreen = $("#crisper_e").clickShow(lastExpandScreen);
+        $(currPage).hide();
+        prevPage = currPage;
+        currPage = $("#expand_screen").clickShow(currPage);
+        currItem = "Chicken Crispers";
+        itemOrderNote = "#crisper_ordered";
+        ;
+    });
+
+    $("#bacon_shrimp").click(function () {
+        lastExpandScreen = $("#bacon_shrimp_e").clickShow(lastExpandScreen);
+        $(currPage).hide();
+        prevPage = currPage;
+        currPage = $("#expand_screen").clickShow(currPage);
+        currItem = "Bacon Shrimp";
+        itemOrderNote = "#bacon_shrimp_ordered";
+        ;
+    });
+
+    $("#invis_drink").click(function () {
+        lastExpandScreen = $("#invis_drink_e").clickShow(lastExpandScreen);
+        $(currPage).hide();
+        prevPage = currPage;
+        currPage = $("#expand_screen").clickShow(currPage);
+        currItem = "Invisible Drink";
+        itemOrderNote = "#invis_drink_ordered";
+        ;
+    });
+
+    $("#wine").click(function () {
+        lastExpandScreen = $("#wine_e").clickShow(lastExpandScreen);
+        $(currPage).hide();
+        prevPage = currPage;
+        currPage = $("#expand_screen").clickShow(currPage);
+        currItem = "Red Wine";
+        itemOrderNote = "#wine_ordered";
+        ;
+    });
+
+    $("#steak").click(function () {
+        lastExpandScreen = $("#steak_e").clickShow(lastExpandScreen);
+        $(currPage).hide();
+        prevPage = currPage;
+        currPage = $("#expand_screen").clickShow(currPage);
+        currItem = "Rib-eye Steak";
+        itemOrderNote = "#steak_ordered";
+        ;
+    });
+
+    $("#fish_chips").click(function () {
+        lastExpandScreen = $("#fish_chips_e").clickShow(lastExpandScreen);
+        $(currPage).hide();
+        prevPage = currPage;
+        currPage = $("#expand_screen").clickShow(currPage);
+        currItem = "Fish and Chips";
+        itemOrderNote = "#fish_chips_ordered";
+        ;
+    });
+
+    $("#lemon_pie").click(function () {
+        lastExpandScreen = $("#lemon_pie_e").clickShow(lastExpandScreen);
+        $(currPage).hide();
+        prevPage = currPage;
+        currPage = $("#expand_screen").clickShow(currPage);
+        currItem = "Lemon Pie";
+        itemOrderNote = "#lemon_pie_ordered";
+        ;
+    });
+
+    $("#shortcake").click(function () {
+        lastExpandScreen = $("#shortcake_e").clickShow(lastExpandScreen);
+        $(currPage).hide();
+        prevPage = currPage;
+        currPage = $("#expand_screen").clickShow(currPage);
+        currItem = "Shortcake";
+        itemOrderNote = "#shortcake_ordered";
+        ;
+    });
+
+
+    //back button for expanded food items, brings back to current menu shown
+    $("#back").click(function () {
+        $("#expand_screen").hide();
+        $(prevPage).show();
+        currPage = prevPage;
+        ;
+    });
+
+
+    //adds items to orders, current max is 8 different per order, if exceeded makes a new order automatically
+    $("#add_order").click(function () {
+        var currOrder = null;
+        var currList = null;
+
+        if (orderCount === 0) {
+            orderCount = 1;
+            updateButtons();
+            currOrder = firstOrder;
+            currList = $("#order1_list");
+        } 
+        else {
+
+            currOrder = firstOrder;
+            currList = $("#order1_list");
+        }
+
+        var searchNode = currOrder.search(currItem);
+        if (searchNode == -1) {
+            currOrder.add(currItem);
+            itemCount++;
+            currList.append("<li id=\"" + currItem.replace(/ /g, "_") + "\">" + currItem + "</li>");
+        } 
+        else {
+            currOrder.increment(searchNode);
+            $("#" + currItem.replace(/ /g, "_")).text(currItem + " ... x" + searchNode.count);
+        }
+
+        $(itemOrderNote).show();
+        $(currPage).hide();
+        $(prevPage).show();
+        currPage = prevPage;
+        ;
+    });
+
+    //make new orders as the buttons are clicked
+    $("#new_order_two").click(function () {
+        if (!orderBtnDisable) {
+            orderCount = 2;
+            $("#new_order_two").hide();
+            $("#order_two").show();
+            $("#new_order_three").show();
+        }
+        ;
+    });
+
+    $("#new_order_three").click(function () {
+        if (!orderBtnDisable) {
+            orderCount = 3;
+            $("#new_order_three").hide();
+            $("#order_three").show();
+        }
+        ;
+    });
+
+
+    //brings up confirmation screen that submits order, submitting order resets orders
+    $("#submit_order").click(function () {
+        if (!orderBtnDisable) {
+            naviBtnDisable = true;
+            orderBtnDisable = true;
+            $("#order_confirm").show();
+
+            $("#order_c_no").click(function () {
+                naviBtnDisable = false;
+                orderBtnDisable = false;
+                $("#order_confirm").hide();
+            });
+
+            $("#order_c_yes").click(function () {
+                naviBtnDisable = false;
+                orderBtnDisable = false;
+                $("#order_confirm").hide();
+                $("#order_two").hide();
+                $("#order_three").hide();
+                $("#order_four").hide();
+                $("#new_order_two").show();
+                $("#new_order_three").hide();
+                $("#new_order_four").hide();
+                currPage = $("#order_submitted").clickShow(currPage);
+
+                lastNav = null;
+                $(".ordered").hide();
+                orderCount = 0;
+                itemCount = 0;
+                orderSubmitted = true;
+                updateButtons();
+                ;
+            });
+
+        }
+        ;
+    });
+
+    //update order and bills button, see if they should be disabled or regular color
+    function updateButtons() {
+        if (orderCount > 0)
+            $("#order").css("background-color", regColor);
+        else
+            $("#order").css("background-color", disabled);
+
+        if (orderSubmitted)
+            $("#bills").css("background-color", regColor);
+        else
+            $("#bills").css("background-color", disabled);
+    }
+    ;
+
+    //Takes the lastClick which is last clicked button, changes it to non hightlighted color, takes new clicked button and highlights it
+    $.fn.clickHilite = function (lastClick) {
+        if (lastClick != null) {
+            $(lastClick).css("background-color", regColor);
+        }
+        lastClick = this;
+        $(this).css("background-color", hilite);
+        return lastClick;
+    };
+
+
+
+    //Takes the lastClick which is last clicked screen, hides it, takes new clicked screen and shows it
+    $.fn.clickShow = function (lastClick) {
+        if (lastClick != null) {
+            $(lastClick).hide();
+        }
+        lastClick = this;
+        $(this).show();
+        return lastClick;
+    };
+});
---
+++ public_html/linkedList.js
@@ -0,0 +1,109 @@
+function Node(data) {
+    this.data = data; //data ie name of food
+    this.elemnt = null; //html element to display in order
+    this.count = 1; //qty of food
+    this.next = null; //next node pointer
+}
+
+
+function LinkedList() {
+    this.traverseNode = null;
+    this.size = 0;
+    this.head = null;
+    this.last = null;
+}
+
+//Traverse the linked list -- use initTraverse to begin or reset the traversal
+LinkedList.prototype.traverse = function () {
+    var data = this.traverseNode.data;
+    this.traverseNode = this.traverseNode.next;
+    return data;
+};
+
+LinkedList.prototype.initTraverse = function () {
+    this.traverseNode = this.head;
+};
+
+//adds node returns the node
+LinkedList.prototype.add = function (value) {
+    var node = new Node(value),
+            currentNode = this.head;
+
+    if (!currentNode) {
+        this.head = node;
+        this.last = node;
+        this.size++;
+    } else {
+        this.last.next = node;
+        this.last = node;
+        this.size++;
+    }
+
+    return node;
+};
+
+//returns -1 if not found, otherwise returns the found node
+LinkedList.prototype.search = function (data) {
+    var currentNode = this.head;
+    var found = false;
+    while (currentNode != null && !found) {
+        if (data === currentNode.data) {
+            found = true;
+        } 
+        else {
+            currentNode = currentNode.next;
+        }
+    }
+
+
+    if (!found) {
+        return -1;
+    } else {
+        return currentNode;
+    }
+};
+
+//returns -1 if not found, otherwise removes the node
+LinkedList.prototype.remove = function (data) {
+    var currentNode = this.head;
+    var prevNode = null;
+    var found = false;
+
+    while (currentNode != null && !found) {
+        if (data == currentNode.data) {
+            found = true;
+        }
+        prevNode = currentNode;
+        currentNode = currentNode.next;
+    }
+
+    if (found) {
+        if (prevNode = null) {
+            this.head = this.head.next;
+        } else {
+            prevNode.next = currentNode.next;
+        }
+
+        this.size--;
+        return currentNode;
+    }
+    return -1;
+};
+
+LinkedList.prototype.increment = function (node) {
+    if (node != null)
+        node.count++;
+};
+
+LinkedList.prototype.decrement = function (node) {
+    if (node != null)
+        node.count--;
+};
+
+LinkedList.prototype.getElement = function (node) {
+    return this.elemnt;
+};
+
+LinkedList.prototype.setElement = function (node, value) {
+    this.elemnt = value;
+};
